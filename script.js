ab339fdcfe8f5de6b80c49b026e847f3e70b8d9d
philip <anlanderp@gmail.com>
2019-02-07 10:51:23 +0100

    10.51

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
 let container = document.getElementsByClassName('.container');
 let addlist = document.getElementById('add-a-list-btn');
-let listbox = document.getElementById('save-listbox')
-let input = document.getElementsByClassName('list-input-box');
-let savebtn = document.getElementsByClassName('save-btn');
+let listbox = document.getElementById('save-listbox');
+let i;
+
 
 
 function createlist(){
   let div = document.createElement("div");
   let box = document.createElement('div');
   let listinput = document.createElement('input');
-  let listbtn = document.createElement('button');
-  listbtn.innerHTML = "Save";
+  let savebt = document.createElement('button');
+  let addlist = document.createElement('button');
+  savebt.innerHTML = "Save";
+
 
   div.classList.add("container");
   box.classList.add("save-list-box");
   listinput.classList.add("input");
-  listbtn.classList.add("savebtn");
+  savebt.classList.add("savebtn");
   console.log(div);
 
-
+  div.setAttribute("style", "display: grid");
   div.appendChild(box);
-  div.appendChild(listinput);
-  div.appendChild(listbtn);
+  box.appendChild(listinput);
+  box.appendChild(savebt);
   document.body.appendChild(div);
 
 }
 
-
-
-
-/*
-
-for (var value = 0; value < value.length; value+= 1) {
-  div.setAttribute("class", "container");
+function newlist (){
+  let inputlist = document.createElement("input");
+  let inputValue = document.getElementsByClassName('input');
+  let x = document.createTextNode(inputValue);
+  inputlist.appendChild(x);
+  if(inputValue == ''){
+    console.log(inputlist);
+    alert("Must name list!")
+  } else {
+    document.getElementsByClassName("input").value = "";
+  }
 }
-
-
-
-  console.log(createlist);
-}
-
-
-*/
